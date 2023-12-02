0fabd378a371e57028da44bbcc71e1b7e25000b5
Stephen Li <stephenli.ca@gmail.com>
2023-12-02 03:15:46 -0500

    wip

---
+++ tests/unit/utils/romanToInt.test.ts
@@ -0,0 +1,13 @@
+import { romanToInt } from '@/utils/romanToInt.js'
+import { expect, test } from 'vitest'
+
+test.each([
+    ['I', 1],
+    ['III', 3],
+    ['LVIII', 58],
+    ['MCMXCIV', 1994],
+    ['XII', 12],
+    ['XXVII', 27],
+])('%s should equal %i', (roman, expectedValue) => {
+    expect(romanToInt(roman)).toBe(expectedValue)
+})
