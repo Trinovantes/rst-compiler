c50eb163e6a60417aec9ab65d91fc7a5d5fafec2
Stephen Li <stephenli.ca@gmail.com>
2023-12-12 20:51:09 -0500

    wip

--- tests/unit/Parser/Block/SimpleTable.test.ts
+++ tests/unit/Parser/Block/SimpleTable.test.ts
@@ -2,19 +2,106 @@ import { test } from 'vitest'
 import { expectDocument } from '../../../fixtures/expectDocument.js'
 import { RstNodeType } from '@/Parser/RstNode.js'
 
-test('simple table', () => {
+test.only('simple table', () => {
+    const input = `
+        =====  =====
+        col 1  col 2
+        test   test
+        =====  =====
+    `
+
+    expectDocument(input, [
+        {
+            type: RstNodeType.Table,
+        },
+    ])
+})
+
+test('simple table must have at least 2 columns, otherwise it will be parsed as section', () => {
+    const input = `
+        =====
+        col 1
+        test
+        =====
+    `
+
+    expectDocument(input, [
+        {
+            type: RstNodeType.Table,
+        },
+    ])
+})
+
+test('simple table with header separator', () => {
     const input = `
         =====  =====
         col 1  col 2
         =====  =====
-        1      Second column of row 1.
-        2      Second column of row 2.
-               Second line of paragraph.
-        3      - Second column of row 3.
+        test   test
+        =====  =====
+    `
 
-               - Second item in bullet
-                 list (row 3, column 2).
-        \\      Row 4; column 1 will be empty.
+    expectDocument(input, [
+        {
+            type: RstNodeType.Table,
+        },
+    ])
+})
+
+test('when there is a line break after header separator, it breaks stops the table parsing after the header', () => {
+    const input = `
+        =====  =====
+        col 1  col 2
+        =====  =====
+
+        test   test
+        =====  =====
+    `
+
+    expectDocument(input, [
+        {
+            type: RstNodeType.Table,
+        },
+    ])
+})
+
+test('TODO', () => {
+    const input = `
+        =====  =====
+        col 1  col 2
+        =====  =====
+               test
+        test   test
+        =====  =====
+    `
+
+    expectDocument(input, [
+        {
+            type: RstNodeType.Table,
+        },
+    ])
+})
+
+test('simple table with empty cell not in first column', () => {
+    const input = `
+        =====  =====
+        col 1  col 2
+        test
+        =====  =====
+    `
+
+    expectDocument(input, [
+        {
+            type: RstNodeType.Table,
+        },
+    ])
+})
+
+test('when there is an empty cell in first column, it should not be a new row', () => {
+    const input = `
+        =====  =====
+        col 1  col 2
+        test
         =====  =====
     `
 
