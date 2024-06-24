c65f17fe7e4cc86790b4eeacf346103c1a82326d
Stephen Li <stephenli.ca@gmail.com>
2024-06-23 21:47:52 -0400

    Print ast first

--- tests/playground/run.ts
+++ tests/playground/run.ts
@@ -1,4 +1,4 @@
-import { RstCompiler, RstToHtmlCompiler, RstToMdCompiler } from '@/RstCompiler.js'
+import { RstToHtmlCompiler, RstToMdCompiler } from '@/RstCompiler.js'
 import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
@@ -7,17 +7,19 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const rst = fs.readFileSync(path.join(dirname, 'playground.rst')).toString('utf-8')
 
+const htmlCompiler = new RstToHtmlCompiler()
+const mdCompiler = new RstToMdCompiler()
+
 console.info('-'.repeat(80))
-const html = new RstToHtmlCompiler().compile(rst)
+const parserOutput = htmlCompiler.parse(rst)
+console.info(parserOutput.root.toString())
+
+console.info('-'.repeat(80))
+const html = htmlCompiler.compile(parserOutput)
 console.info(html.body)
 console.info(html.downloads)
 
 console.info('-'.repeat(80))
-const md = new RstToMdCompiler().compile(rst)
+const md = mdCompiler.compile(parserOutput)
 console.info(md.body)
 console.info(md.downloads)
-
-console.info('-'.repeat(80))
-const compiler = new RstCompiler()
-const parserOutput = compiler.parse(rst)
-console.info(parserOutput.root.toString())
