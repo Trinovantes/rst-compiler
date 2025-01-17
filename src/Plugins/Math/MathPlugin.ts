36af0d028048377c79223a8c3631b01bcd42d814
Stephen Li <stephenli.ca@gmail.com>
2025-01-17 17:18:05 -0500

    Upgrade dependencies

--- src/Plugins/Math/MathPlugin.ts
+++ src/Plugins/Math/MathPlugin.ts
@@ -4,7 +4,7 @@ import { createRstCompilerPlugins } from '@/RstCompilerPlugin.js'
 import katex from 'katex'
 
 const globalHeaderKey = 'katex'
-const globalHeader = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.11/dist/katex.min.css" integrity="sha384-nB0miv6/jRmo5UMMR1wu3Gz6NLsoTkbqJghGIsx//Rlm+ZU03BU6SQNC66uf4l5+" crossorigin="anonymous">'
+const globalHeader = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css" integrity="sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib" crossorigin="anonymous">'
 
 // ----------------------------------------------------------------------------
 // MARK: Directive
